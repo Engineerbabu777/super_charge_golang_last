9c3e12762f92c0bc1a033b695cb78a2465787d24
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-22 21:04:22 +0500

    day 4 completed and added go routnes and channels

--- day_4/main.go
+++ day_4/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// Pointer()
 	// CalculateValues(5,5)
+
+	waitingch := make(chan int)
+
+	go FetchResource(&waitingch, 1)
+	go FetchResource(&waitingch, 2)
+	go FetchResource(&waitingch, 3)
+
+	// time.Sleep(time.Second * 5)
+
+	<-waitingch
+	<-waitingch
+	<-waitingch
+
 }
 
 type Player struct {
@@ -25,7 +41,15 @@ func Pointer() {
 	fmt.Println(player.HP)
 }
 
-
 func CalculateValues(x int, y int) int {
 	return x + y
+}
+
+func FetchResource(waitingch *chan int, GR int) {
+	fmt.Println("Fetching resource ", GR)
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("Waiting for 2 secs,  over and out!")
+	// return "Hello"
+	*waitingch <- GR
 }
--- day_4/main_test.go
+++ day_4/main_test.go
@@ -9,15 +9,18 @@ func TestCalculateValues(t *testing.T) {
 	// t.Fail()
 	var (
 		expected = 201
-		a = 10
-		b = 10
+		a        = 10
+		b        = 10
 	)
 
-	isOk := CalculateValues(a,b);
-	
+	isOk := CalculateValues(a, b)
+
 	if isOk != expected {
-	    t.Errorf("expected %d but got %d",expected,isOk)
+		t.Errorf("expected %d but got %d", expected, isOk)
 	}
 
 	fmt.Println("Hello from test")
 }
+
+// in the current dir run TestCalculateValues func
+// go test ./ -v -run TestCalculateValues
